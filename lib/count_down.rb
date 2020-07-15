e89bca0cb6e0d733e2f5dec04bd9353e225462c4
Anney Park <anneypark@gmail.com>
2020-07-15 20:55:19 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -1,6 +1,5 @@
-magic_exit_number = 7
-count = 0 
-while count < 10 && count != magic_exit_number do
-  puts "I am the #{count}, I love to count!" #work
-  count = count + 1 
-end
+count = log 
+while count < 1 do
+  puts "Happy New Year!"
+  count = count - 1 
+end 
