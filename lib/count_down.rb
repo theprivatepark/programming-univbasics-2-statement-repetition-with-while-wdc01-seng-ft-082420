4ed9ae07d8b783d2418bdc5593e983cf175ac33d
Anney Park <anneypark@gmail.com>
2020-07-15 20:32:06 +0000

    Automatically backed up by Learn

--- lib/count_down.rb
+++ lib/count_down.rb
@@ -2,6 +2,6 @@ magic_exit_number = 7
 count = 0 
 while count < 10 do
   break if count == magic_exit_number
-  puts "I am the #{count}, I love to count! #Work
+  puts "I am the #{count}, I love to count!
   count = count + 1 
 end
